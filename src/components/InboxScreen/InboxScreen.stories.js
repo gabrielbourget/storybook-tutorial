e09c0354d3b44487f852fcc784fdaababe113c86
Gabriel Bourget <gaboxa@syntronic.com>
2021-02-20 14:36:45 -0500

    Deploy to Chromatic

--- src/components/InboxScreen/InboxScreen.stories.js
+++ src/components/InboxScreen/InboxScreen.stories.js
@@ -1,26 +1,32 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { action } from "@storybook/addon-actions";
-import { PureInboxScreen } from "./InboxScreen";
-import * as TaskListStories from "../TaskList/TaskList.stories";
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Provider } from 'react-redux';
 
-const store = {
-  getState: () => ({ tasks: TaskListStories.Default.args.tasks }),
-  subscribe: () => 0,
-  dispatch: action("dispatch"),
-};
+import { PureInboxScreen } from './InboxScreen';
+import * as TaskListStories from '../TaskList/TaskList.stories';
 
 const storyConfig = {
   component: PureInboxScreen,
-  title: "InboxScreen",
-  deecorators: [(story) => <Provider store={store}>{story()}</Provider>]
+  title: 'InboxScreen',
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
 };
 
 export default storyConfig;
 
+// A super-simple mock of a redux store
+const store = {
+  getState: () => {
+    return {
+      tasks:TaskListStories.Default.args.tasks,
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action('dispatch'),
+};
+
 const Template = (args) => <PureInboxScreen {...args} />;
-
 export const Default = Template.bind({});
-
 export const Error = Template.bind({});
-Error.args = { error: "Something went wrong while trying to retrieve tasks" };
+Error.args = {
+  error: "Something",
+};
